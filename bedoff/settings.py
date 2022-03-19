c5f99b365b0dac10bf2638912c71bad214e94c96
nafisdev <nafis.mohammad65@gmail.com>
2022-03-19 11:34:44 +0530

    db details

--- bedoff/settings.py
+++ bedoff/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'bedoff.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bedoff_db',
+        'USER': 'bedoff',
+        'PASSWORD': 'bedoff_db',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3310',
     }
 }
+
 
 
 # Password validation
