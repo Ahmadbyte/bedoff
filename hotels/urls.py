c3fa7fc4ea6e0774976406679c1235e6c6330876
nafisdev <nafis.mohammad65@gmail.com>
2022-04-23 13:10:04 +0530

    hotel get by id api impl

--- hotels/urls.py
+++ hotels/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 
 from hotels.views import hotel_views
 
-urlpatterns = [path("", hotel_views.HotelsModelViewSet.as_view({"get": "list", "post": "create"}))]
+urlpatterns = [path("", hotel_views.HotelsModelViewSet.as_view({"get": "list", "post": "create"})),
+               path("<int:pk>", hotel_views.HotelsModelViewSet.as_view({"get": "retrieve", "put": "update"}))]
+
