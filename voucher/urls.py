1c27fd14ee41a11d8e457e7378ebff80ddcfa34c
nafisdev <nafis.mohammad65@gmail.com>
2022-04-30 23:07:48 +0530

    sample pdf generration using pdfkit

--- voucher/urls.py
+++ voucher/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from voucher import views
 
 urlpatterns = [
-    path("pdf", views.create_pdf()),
+    path("", views.create_pdf),
 ]
--- voucher/views.py
+++ voucher/views.py
@@ -5,7 +5,7 @@ import pdfkit
 from django.http import HttpResponse
 from django.template import loader
 
-def create_pdf():
+def create_pdf(request):
     html = loader.render_to_string('test.html', {})
     output= pdfkit.from_string(html, output_path=False)
     response = HttpResponse(content_type="application/pdf")
