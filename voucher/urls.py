3a33a23295597e3e5be955b1a90c9819016eef97
waqas <waqas.hamid@myparkplus.com>
2022-05-01 18:11:55 +0530

    Tested PDF that the rendering works

--- voucher/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from voucher import views
-
-urlpatterns = [
-    path("", views.create_pdf),
-]
--- voucher/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from voucher.service.process import html_to_pdf
-
-
-def create_pdf(request):
-
-    return html_to_pdf()
