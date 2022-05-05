6811f0d39e6f928ee2b80a18631d91e7677152bf
waqas <waqas.hamid@myparkplus.com>
2022-05-05 18:37:00 +0530

    added filter on name

--- hotels/views/hotel_views.py
+++ hotels/views/hotel_views.py
@@ -1,3 +1,5 @@
+from url_filter.integrations.drf import DjangoFilterBackend
+
 from base.views import BaseModelViewSet
 from hotels import models as hotel_models
 from hotels.serializers import hotel_serializers
@@ -11,3 +13,7 @@ class HotelsModelViewSet(BaseModelViewSet):
     lookup_field = "uid"
     queryset = hotel_models.Hotel.objects.all().order_by("-created_at")
     serializer_class = hotel_serializers.HotelModelSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = [
+        "name",
+    ]
